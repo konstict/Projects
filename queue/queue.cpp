99b2ab5cf56235370459e43b01950e14bad39ed0
kostik <97781288+konstict@users.noreply.github.com>
2025-01-02 00:01:19 +0300

    Add files via upload

---
+++ queue/queue.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+template<typename T>
+class queueNode{
+    public:
+    T val;
+    queueNode<T>* next;
+    queueNode<T>* previos;
+    queueNode(T param){
+        val = param;
+        next = nullptr;
+        previos = nullptr;
+    }
+};
+
+template<typename T>
+class queue{
+    private:
+    queueNode<T>* begin;
+    queueNode<T>* end;
+
+    public:
+    queue(){
+        this->begin = nullptr;
+        this->end = nullptr;
+    }
+
+    bool empty(){
+        return begin==nullptr;
+    }
+
+    void push(T param){
+        queueNode<T>* newEnd = new queueNode<T>(param);
+        newEnd->previos = end;
+        if(this->empty() == true){
+            this->begin = newEnd;
+        }
+        else{
+            end->next = newEnd;
+        }
+        this->end = newEnd;
+    }
+
+    queueNode<T>* peek(){
+        return this->begin;
+    }
+
+    void pop(){
+        if(this->empty() == false){
+            begin->next->previos = nullptr;
+        }
+        this->begin = begin->next;
+    }
+};
+
+int main(){
+    queue<int> a;
+    a.push(1);
+    a.push(2);
+    a.push(3);
+    a.pop();
+    a.pop();
+    cout << a.peek()->val;
+    return 0;
+}
