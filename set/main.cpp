0769a111c0dec5acfda9285cd83896ea9dc21691
kostik <97781288+konstict@users.noreply.github.com>
2025-01-08 20:55:30 +0000

    create set

---
+++ set/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+template<typename T>
+class set{
+private:
+    vector<T> setList;
+
+public:
+    set(){}
+
+    set(T list[]){
+        
+    }
+
+    int size(){
+        return this->list.size;
+    }
+};
+
+int main(){
+
+    return 0;
+}
